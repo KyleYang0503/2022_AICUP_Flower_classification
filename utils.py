2f87d9500c14a0a9e61295b680838fb242ddad2f
KyleYang0503 <92350915+KyleYang0503@users.noreply.github.com>
2022-06-17 17:02:15 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 29 12:24:38 2022
+
+@author: Kyle
+"""
+
+import os
+import sys
+import json
+import pickle
+import random
+
+import torch
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
+
+def read_split_data(root: str, val_rate: float = 0):
+    random.seed(0)  
+    assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
+
+    flower_class = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
+    flower_class.sort()
+    class_indices = dict((k, v) for v, k in enumerate(flower_class))
+    json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    train_images_path = []  
+    train_images_label = []  
+    val_images_path = []  
+    val_images_label = []  
+    every_class_num = []  
+    supported = [".jpg", ".JPG", ".png", ".PNG"] 
+    for cla in flower_class:
+        cla_path = os.path.join(root, cla)
+        images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)
+                  if os.path.splitext(i)[-1] in supported]
+        image_class = class_indices[cla]
+        every_class_num.append(len(images))
+        val_path = random.sample(images, k=int(len(images) * val_rate))
+
+        for img_path in images:
+            if img_path in val_path:  
+                val_images_path.append(img_path)
+                val_images_label.append(image_class)
+            else:  
+                train_images_path.append(img_path)
+                train_images_label.append(image_class)
+
+    print("{} images were found in the dataset.".format(sum(every_class_num)))
+    print("{} images for training.".format(len(train_images_path)))
+    print("{} images for validation.".format(len(val_images_path)))
+
+    return train_images_path, train_images_label, val_images_path, val_images_label
+
+
+def plot_data_loader_image(data_loader):
+    batch_size = data_loader.batch_size
+    plot_num = min(batch_size, 4)
+
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), json_path + " does not exist."
+    json_file = open(json_path, 'r')
+    class_indices = json.load(json_file)
+
+    for data in data_loader:
+        images, labels = data
+        for i in range(plot_num):
+            # [C, H, W] -> [H, W, C]
+            img = images[i].numpy().transpose(1, 2, 0)
+            img = (img * [0.229, 0.224, 0.225] + [0.485, 0.456, 0.406]) * 255
+            label = labels[i].item()
+            plt.subplot(1, plot_num, i+1)
+            plt.xlabel(class_indices[str(label)])
+            plt.xticks([])  
+            plt.yticks([])  
+            plt.imshow(img.astype('uint8'))
+        plt.show()
+
+
+def write_pickle(list_info: list, file_name: str):
+    with open(file_name, 'wb') as f:
+        pickle.dump(list_info, f)
+
+
+def read_pickle(file_name: str) -> list:
+    with open(file_name, 'rb') as f:
+        info_list = pickle.load(f)
+        return info_list
+
+
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+    loss_function = torch.nn.CrossEntropyLoss()
+    accu_loss = torch.zeros(1).to(device)  
+    accu_num = torch.zeros(1).to(device)   
+    optimizer.zero_grad()
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        loss.backward()
+        accu_loss += loss.detach()
+
+        data_loader.desc = "[train epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
+                                                                               accu_loss.item() / (step + 1),
+                                                                               accu_num.item() / sample_num)
+
+        if not torch.isfinite(loss):
+            print('WARNING: non-finite loss, ending training ', loss)
+            sys.exit(1)
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
+    loss_function = torch.nn.CrossEntropyLoss()
+
+    model.eval()
+
+    accu_num = torch.zeros(1).to(device)   
+    accu_loss = torch.zeros(1).to(device)  
+
+    sample_num = 0
+    data_loader = tqdm(data_loader, file=sys.stdout)
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        accu_loss += loss
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}".format(epoch,
+                                                                               accu_loss.item() / (step + 1),
+                                                                               accu_num.item() / sample_num)
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+import pandas as pd
+import shutil
+from PIL import Image
+import numpy as np
+
+def train_by_class(label_path,data_path,target):
+    df = pd.read_csv(label_path,index_col = 'filename')
+    i=0
+    for x in list(df.index.values):
+        label = df.loc[x].values[0]
+        if not os.path.exists(target +'/%d'%label):
+            os.makedirs(target + '/%d'%label)
+        shutil.copy(data_path + '/' + x,"./data/train" + '/%d/'%label + x)
+        '''
+        if i%10==0:
+            shutil.copy(data_path + '/' + x,"./data/train_0.9/test" + '/%d/'%label + x)
+        else:
+            shutil.copy(data_path + '/' + x,"./data/train_0.9/train" + '/%d/'%label + x)
+        i+=1
+        '''
+def gasuss_noise(image, mean=0, var=0.001):
+    image = np.array(image/255, dtype=float)
+    noise = np.random.normal(mean, var ** 0.5, image.shape)
+    out = image + noise
+    if out.min() < 0:
+        low_clip = -1.
+    else:
+        low_clip = 0.
+    out = np.clip(out, low_clip, 1.0)
+    out = np.uint8(out*255)
+    return out
+
+def rotate_enhance(data_path):
+    for class_ in os.listdir(data_path):
+        dir_path = data_path + '/%s'%class_
+        image_list = os.listdir(dir_path)
+        print(class_)
+        for item in image_list:
+            img = Image.open(dir_path + "/%s"%item)
+            angle = "LR"
+            op = [Image.FLIP_LEFT_RIGHT]
+            for i in op:
+                img = gasuss_noise(img)
+                img.save(dir_path +"/%s_copy.jpg"%item)
+                
+
+
+    
+    
+    
+    
+    
+    
